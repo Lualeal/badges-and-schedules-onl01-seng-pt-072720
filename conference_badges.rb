633fbfcb88cff2919d29615330d455dd15ed7a02
Luana Leal <luana.leal.eq@gmail.com>
2020-08-05 04:17:52 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,18 @@
 # Write your code here.
+
+
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end
+
+def print_name(first_name)
+  puts first_name
+end
+
+def batch_badge_creator(attendees)
+  badge_messages = []
+  attendees.each do |attendee|
+    badge_messages << badge_maker(attendee)
+  end 
+  badge_messages
+end 
