0d382bb80058e5f9f7237c205d009d7e815bf642
Luana Leal <luana.leal.eq@gmail.com>
2020-08-05 04:44:29 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -21,6 +21,6 @@ def assign_rooms(attendees)
   room_assignments = []
   attendees.each_with_index do |attendee, index|
     room_assignments << "Hello, #{attendee}! You'll be assigned to room #{index + 1}!"
-end
+  end
   room_assignments
 end 
